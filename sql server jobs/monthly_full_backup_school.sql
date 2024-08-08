e65f4d01397f84b8fa107210eb4910bfc36d139f
lkv971 <leeroyvaillant@outlook.com>
2024-08-08 10:09:02 -0400

    Add sql server job scripts

---
+++ sql server jobs/monthly_full_backup_school.sql
@@ -0,0 +1,71 @@
+USE [msdb]
+GO
+
+/****** Object:  Job [monthly_full_backup_school]    Script Date: 8/8/2024 10:08:35 AM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 8/8/2024 10:08:35 AM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'monthly_full_backup_school', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'Backup of theSchoolDB database every 2 months at 5:00 AM. Backup occurs on the 7th day of the month.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'LAPTOP-E44HM49P\ACER', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [Full Backup]    Script Date: 8/8/2024 10:08:35 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Full Backup', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'BACKUP DATABASE SchoolDB
+TO DISK ''C:\Users\ACER\Documents\GitHub\School\backups\Full\full_back_up_school_'' + FORMAT(GETDATE(), ''yyyymmdd'') + ''.bak''
+WITH INIT
+;
+GO', 
+		@database_name=N'SchoolDB', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'full backup scheduling', 
+		@enabled=1, 
+		@freq_type=16, 
+		@freq_interval=8, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=2, 
+		@active_start_date=20240808, 
+		@active_end_date=99991231, 
+		@active_start_time=50000, 
+		@active_end_time=235959, 
+		@schedule_uid=N'c61c9ef5-4725-4f51-ad52-91589a9bf937'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
